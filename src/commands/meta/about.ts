4e15f6b805f476fa002f96d1347415cbaef68cea
Ken <ikenonline@gmail.com>
2022-01-11 20:32:30 +0700

    edit about

--- src/commands/meta/about.ts
+++ src/commands/meta/about.ts
@@ -27,12 +27,9 @@ export default class About extends Command {
 
     async run(source: CommandSource): Promise<SendMessage | undefined> {
         const me = (await client.users.fetch("127393188729192448")).tag
-        return sendMessage(source, `This is an open-source bot created by @${me}. The source-code is available on GitHub: <https://github.com/Tibowl/HuTao>.
-Data is compiled from a variety of sources (including but not limited to official forum posts/e-mails/videos/posts elsewhere and Genshin Impact Wiki <https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki>).
-© All rights reserved by miHoYo. Other properties belong to their respective owners.
+        return sendMessage(source, `Xin Chào ! ...
 
-You can invite this bot with <https://discord.com/oauth2/authorize?client_id=826550363355086918&scope=bot+applications.commands&permissions=313344>.
-
-Feel free to contact me in case there's an error/need support/got a suggestion via <https://discord.gg/BM3Srp8j8G> or via GitHub issues (link above).`)
+You can invite this bot with <https://discord.com/oauth2/authorize?client_id=>.
+`)
     }
 }
